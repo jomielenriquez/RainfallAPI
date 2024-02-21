01d5a2cf4abd3f805a59db19070b698c5b3a4748
Jomiel <enriquez.jliquigan@gmail.com>
2024-02-21 10:26:16 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "1.0",
+        Title = "Rainfall API",
+        Contact = new OpenApiContact
+        {
+            Name = "Sorted",
+            Url = new Uri("https://www.sorted.com"),
+        },
+        Description = "An API which provides rainfall reading data"
+    });
+
+    c.AddServer(new OpenApiServer { Url = "http://localhost:3000", Description = "Rainfall Api" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
